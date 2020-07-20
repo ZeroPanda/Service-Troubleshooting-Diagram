e35c69c232e92ac208303fc6d896de51b40a1b70
Shrey Shah <41659522+ZeroPanda@users.noreply.github.com>
2020-07-20 16:11:58 -0400

    Create init.R

---
+++ init.R
@@ -0,0 +1,10 @@
+my_packages = c("rmarkdown","httr","flexdashboard","tidyverse","readxl","readr","collapsibleTree")
+
+install_if_missing = function(p){ 
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+   }
+ }
+
+invisible(sapply(my_packages, install_if_missing))
+invisible(devtools::install_github("rstudio/pool"))
